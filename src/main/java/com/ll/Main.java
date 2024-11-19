31f78032a3efd2821ec46f545a5c10f797601fb6
seeyeon <gulf72pr@swu.ac.kr>
2024-11-19 14:45:58 +0900

    ë©ë©App step3

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -18,10 +18,36 @@ class App{
         System.out.println("===명언 앱===");
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("명령) ");
 
-        String cmd = sc.nextLine();
-        System.out.println("입력된 명령어: %s".formatted(cmd));
+        while( true ){
+            System.out.print("명령) ");
+
+            String cmd = sc.nextLine();
+            //System.out.println("입력된 명령어: %s".formatted(cmd));
+
+            if (cmd.equals("종료")){
+                break;
+            }else if(cmd.equals("등록")){
+
+                int count=0;
+
+                System.out.print("명언: ");
+                String saying = sc.nextLine();
+
+                System.out.print("작가: ");
+                String author = sc.nextLine();
+
+                count++;
+
+                System.out.println(count+"번 명언이 등록되었습니다.");
+
+            }
+        }
+
+
+
+
+
         sc.close();
 
 
