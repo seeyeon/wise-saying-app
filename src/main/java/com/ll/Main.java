a3beb351c33c294ef2c6bc9bfeab45d6826f5aef
seeyeon <gulf72pr@swu.ac.kr>
2024-11-19 16:24:36 +0900

    App step4

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,53 +2,54 @@ package com.ll;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
 
         App app = new App();
         app.run();
+        
     }
-
 }
 
 class App{
 
+
     public void run() {
 
-        System.out.println("===명언 앱===");
-
+        System.out.println("==명언 앱==");
         Scanner sc = new Scanner(System.in);
+        int count =0;
 
-        while( true ){
+        while(true){
+
             System.out.print("명령) ");
-
             String cmd = sc.nextLine();
-            //System.out.println("입력된 명령어: %s".formatted(cmd));
 
-            if (cmd.equals("종료")){
+
+            if(cmd.equals("종료")){
                 break;
             }else if(cmd.equals("등록")){
 
-                int count=0;
 
-                System.out.print("명언: ");
-                String saying = sc.nextLine();
+                String saying=" ";
+                String author=" ";
 
-                System.out.print("작가: ");
-                String author = sc.nextLine();
+                System.out.print("명언 : ");
+                saying = sc.nextLine();
+
+                System.out.print("작가 : ");
+                author = sc.nextLine();
 
                 count++;
-
-                System.out.println(count+"번 명언이 등록되었습니다.");
+                System.out.println(count + " 번 명언이 등록되었습니다.");
 
             }
+
+
+
+
         }
 
-
-
-
-
-        sc.close();
 
 
     }
